7599c29ee1718fdf048b55236036c0ad7b501950
Katrina Brinson <kbrinso@gmail.com>
2018-01-08 16:44:16 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,6 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   const bod = document.querySelector('body');
+  let index = 0;
+  
   bod.addEventListener('keydown', function(e){
     const key = parseInt(e.detail || e.which);
     
